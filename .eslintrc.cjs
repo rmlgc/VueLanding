d10db2d560398e441932578ce2c1b052d5a754ee
rmlgc <rommelgarcia4b@gmail.com>
2022-12-23 14:03:36 +0000

    config eslint Airbnb

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,21 @@
 require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
+  // /* env added for avoid process.env from playWright*/
+  env: {
+    node: true,
+    commonjs: true,
+  },
   root: true,
   extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
     '@vue/eslint-config-typescript',
-    'plugin:vue/vue3-recommended',
+    'plugin:vue/vue3-strongly-recommended',
     'eslint:recommended',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
+    project: ["./tsconfig.eslint.json"],
   },
 };
--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,47 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
+
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    >
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">
-          Home
-        </RouterLink>
-        <RouterLink to="/about">
-          About
-        </RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <main>
+    <section>
+      <div>
+        <h1 class="text--interact text-center">
+        <span class="text-increase">
+          W</span>
+        <span class="text-reduces">ork
+        </span>
+        <span class="text-increase">I</span>
+        <span class="text-reduces">n
+        </span>
+        <span class="text-increase">
+          P</span>
+        <span class="text-reduces">rogress
+        </span>
+        </h1>
+      </div>
+    </section>
+  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style lang="scss">
+.text{
+  &-increase{
+    font-size: 10rem;
+  .text--interact:hover &,
+  &.text--interact:hover{
+    font-size: 4rem;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  &-reduces{
+    font-size: 0;
+    .text--interact:hover &,
+    &.text--interact:hover{
+      font-size: 4rem;
+    }
   }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  &-center{
+    text-align: center;
   }
 }
 </style>
