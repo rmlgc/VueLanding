71a27fd1b595595a2152a5f0c042c9daadae8c96
rmlgc <rommelgarcia4b@gmail.com>
2022-12-29 17:04:57 +0000

    short text about my career

--- src/App.vue
+++ src/App.vue
@@ -18,27 +18,33 @@ const elPortfolio = ref(null)
 const elPortfolioIsVisible = ref(false)
 const elContact = ref(null)
 const elContactIsVisible = ref(false)
+const pageIsLoad = ref(false)
 
 const isMenuOpen = ref(false)
 
 const scrollTo = (elemento: any) => {
   console.log(elemento)
   let targetElemnt = elemento;
-  if (elemento.$el) {
-    targetElemnt = elemento.$el
+  if (elemento.value) {
+    const {$el}: any = elemento.value;
+    targetElemnt = $el
   }
   targetElemnt.scrollIntoView({behavior: "smooth"})
-
+  isMenuOpen.value = false;
 }
 
-onMounted(()=>{
-  let options = {
-    margin:'10%',
+onMounted(() => {
+  let options = {}
+
+  document.onreadystatechange = () => { //To load the app only after all libraries are loaded
+    if (document.readyState == "complete") {
+      pageIsLoad.value = true;
+      console.log('PAGE LOADED')
+    }
   }
 
   const {$el}: any = elWip.value;
   initIntersection($el, elWipIsVisible, document.querySelector('body'), options)
-  initIntersection(elHome.value, elHomeIsVisible, document.querySelector('body'), options)
   initIntersection(elAboutMe.value, elAboutMeIsVisible, document.querySelector('body'), options)
   initIntersection(elService.value, elServiceIsVisible, document.querySelector('body'), options)
   initIntersection(elResume.value, elResumeIsVisible, document.querySelector('body'), options)
@@ -47,9 +53,9 @@ onMounted(()=>{
 
 })
 
-const initIntersection = (element:any, elementIsVisible:any, elementScroll:any,  newOptions = {})=>{
+const initIntersection = (element: any, elementIsVisible: any, elementScroll: any, newOptions = {}) => {
   let options = {
-    threshold:0.5,
+    threshold: 0.12,
     ...newOptions
   }
 
@@ -68,19 +74,19 @@ const initIntersection = (element:any, elementIsVisible:any, elementScroll:any, 
 
 <template>
   <WipPage id="elWip" ref="elWip" class="content-full"></WipPage>
-  <main>
-    <header @click.self="isMenuOpen=!isMenuOpen" :class="`${isMenuOpen ? 'open' : ''}`">
+  <main v-show="pageIsLoad">
+    <header @click.self="isMenuOpen=!isMenuOpen" :class="`${isMenuOpen ? 'open bg-glass' : ''}`">
       <div
         :class="`header-menu ${isMenuOpen ? 'header-menu-full bg-glass--fat' : 'bg-glass--slim'} bg-glass-secondary `">
-        <div class="header-menu-title">
-          <a href="https://linktr.ee/rommelgc" target="_blank" class="decoration-none text-spacing-none">
+        <div class="header-menu-head text-center">
+          <a href="https://linktr.ee/rommelgc" target="_blank"
+             class="header-menu-title decoration-none text-spacing-none">
             <span class="text-increase">R</span>
             <span class="text-reduces">ommel</span>
             <span class="text-increase">G</span>
             <span class="text-reduces">arcia</span>
             <span class="text-increase">C</span>
             <span class="text-reduces">oronado</span>
-            {{ }}
           </a>
           <div class="header-menu-btn" @click="isMenuOpen=!isMenuOpen">
             <span v-if="isMenuOpen" class="material-symbols-rounded">close</span>
@@ -88,31 +94,27 @@ const initIntersection = (element:any, elementIsVisible:any, elementScroll:any, 
           </div>
         </div>
         <div class="header-menu-content">
-          <a @click="scrollTo(elWip)" :class="`header-menu-link ${elWipIsVisible ? 'active' : ''}`"> <span
-            class="material-symbols-rounded">
-engineering
-</span>Wip</a>
-          <a @click="scrollTo(elHome)" :class="`header-menu-link ${elHomeIsVisible ? 'active' : ''}`"> <span
-            class="material-symbols-rounded">
-home
-</span> Home</a>
-          <a @click="scrollTo(elAboutMe)" :class="`header-menu-link ${elAboutMeIsVisible ? 'active' : ''}`"><span
+          <a @click="scrollTo(elAboutMe)"
+             :class="`header-menu-link ${elAboutMeIsVisible ? 'active bg-glass' : ''}`"><span
             class="material-symbols-rounded">
 person
 </span> About me</a>
-          <a @click="scrollTo(elService)" :class="`header-menu-link ${elServiceIsVisible ? 'active' : ''}`"><span
+          <a style="display: none;" @click="scrollTo(elService)"
+             :class="`header-menu-link ${elServiceIsVisible ? 'active bg-glass' : ''}`"><span
             class="material-symbols-rounded">
 design_services
 </span> Service</a>
-          <a @click="scrollTo(elResume)" :class="`header-menu-link ${elResumeIsVisible ? 'active' : ''}`"><span
+          <a @click="scrollTo(elResume)" :class="`header-menu-link ${elResumeIsVisible ? 'active bg-glass' : ''}`"><span
             class="material-symbols-rounded">
 school
 </span> Resume</a>
-          <a @click="scrollTo(elPortfolio)" :class="`header-menu-link ${elPortfolioIsVisible ? 'active' : ''}`"><span
+          <a @click="scrollTo(elPortfolio)"
+             :class="`header-menu-link ${elPortfolioIsVisible ? 'active bg-glass' : ''}`"><span
             class="material-symbols-rounded">
 important_devices
 </span> Portfolio</a>
-          <a @click="scrollTo(elContact)" :class="`header-menu-link ${elContactIsVisible ? 'active' : ''}`"><span
+          <a @click="scrollTo(elContact)"
+             :class="`header-menu-link ${elContactIsVisible ? 'active bg-glass' : ''}`"><span
             class="material-symbols-rounded">
 contact_mail
 </span> Contact</a>
@@ -120,24 +122,168 @@ contact_mail
       </div>
     </header>
     <div class="main-content">
-      <section id="elHome" ref="elHome" class="section-card">
-        <h2 class="text-h1"> Home </h2>
-      </section>
       <section id="elAboutMe" ref="elAboutMe" class="content section-card">
-        <h2 class="text-h2"> About me </h2>
+        <h2 class="text-h6"> 👋 Hi There? I’m Rommel</h2>
+        <p class="text-body2">
+          I have a 2 year career as a 'web developer' && <span class="inline-block">2 years of learning pregress with my own projects</span>
+        </p>
+        <hr>
+        <p class="text-body2">🧑‍🎓 Professional Formation Grade as Web Application Developer 👨‍💻</p>
+        <p class="text-h6">
+          $ ✨🖥️ Frontend web developer 📐📱
+        </p>
+        <p class="text-h6">
+          $ 📱✨ FullStack web developer 🗄️🖥️
+        </p>
+        <p class="text-body1">
+          $ <span class="text-body2">🖥️🗄️ Backend web developer 💽🖥️</span>
+        </p>
+        <p class="text-body1">
+          $ <small class="text-body2">🧰 Helpdesk Operator 🖱️</small>
+        </p>
+        <hr>
+        <p>
+          🧑‍🎨 my free time:
+        </p>
+        <p class="text-body1">
+          $ <small class="text-body2">👾 Play Videogames 🎲</small>
+        </p>
+        <p class="text-h6">
+          $ 🎥 Content creator 🐒
+        </p>
       </section>
-      <section id="elService" ref="elService" class="content section-card">
+      <section style="display: none;" id="elService" ref="elService" class="content section-card">
         <h2 class="text-h3"> Service </h2>
       </section>
       <section id="elResume" ref="elResume" class="content section-card">
         <h2 class="text-h4"> Experience </h2>
-        <h2 class="text-h5"> Resume </h2>
+        <div class="resume-content">
+          <div class="resume-item resume-item--fullstack">
+            <h3> FullStack</h3>
+            <p>
+              <small>
+                I have little experience with apache servers, just enough to manage a web server. I am currently
+                interested in learning how to manage/deploy serverless web/App
+              </small>
+            </p>
+            <p>
+              I feel more confident designing and developing the client side (<a href="#resumeFrontend"
+                                                                                 :class="`bg-glass active`">FrontEnd</a>)
+            </p>
+            <p>
+              however, developing the server side is always a challenge, it's not my best path, but being constantly
+              learning is rewarding(<a href="#resumeFrontend"
+                                       :class="`bg-glass active`">BackEnd</a>)
+            </p>
+          </div>
+          <div id="resumeFrontend" class="resume-item resume-item--frontend">
+            <h3>FrontEnd</h3>
+            <p class="text-body2">my knowledge:</p>
+            <ol>
+              <li>
+                Html
+              </li>
+              <li>
+                Css
+                <ul>
+                  <li>
+                    scss
+                  </li>
+                  <li>
+                    sass
+                  </li>
+                  <li>
+                    PostCSS
+                  </li>
+                  <li>
+                    less
+                  </li>
+                </ul>
+              </li>
+              <li>
+                Javascript
+                <ul>
+                  <li>
+                    Vue3
+                  </li>
+                  <li>
+                    Angular
+                  </li>
+                  <li>
+                    React
+                  </li>
+                  <li>
+                    Svelt
+                  </li>
+                </ul>
+              </li>
+            </ol>
+          </div>
+          <div class="resume-item resume-item--backend">
+            <h3>Backend</h3>
+            <p class="text-body2">my knowledge</p>
+            <ol>
+              <li>
+                php
+                <ul>
+                  <li>
+                    Framework Laravel
+                  </li>
+                  <li>
+                    Framework CakePhP
+                  </li>
+                </ul>
+              </li>
+              <li>
+                Javascript
+                <ul>
+                  <li>
+                    NodeJS
+                  </li>
+                  <li>
+                    Deno
+                  </li>
+                </ul>
+              </li>
+              <li>
+                others
+                <ul>
+                  <li>
+                    python
+                  </li>
+                  <li>
+                    Java
+                  </li>
+                </ul>
+              </li>
+            </ol>
+          </div>
+        </div>
+        <h2 class=""> Resume </h2>
+        <h3>Academic grade</h3>
+        <h3>Jobs trajectory</h3>
       </section>
-      <section id="elPortfolio" ref="elPortfolio" class="content section-card">
-        <h2 class="text-h6"> Portfolio </h2>
+      <section id="elPortfolio" ref="elPortfolio" class="content section-card wrap-center">
+        <h2 class="text-h6"> Portfolio: </h2>
+        <div>
+          <h3>Amigaria WebApp</h3>
+          <p>Amigaria is an accessible web platform for social integration.</p>
+          <p>developed in group in EOI frontend develop course, doing Agile methods and publish git versions.</p>
+          <a class="active" href="https://amigaria.com/">Amigaria</a>
+        </div>
+        <div>
+          <h3>Vue Dummyportfolio</h3>
+          <p>Work In Progress</p>
+          <p>Simple Web App. render, connect to Rest Apis and list data</p>
+          <p>Developed in Vue, using Quasar VueJs Components. for a simple mobile and desktop style design</p>
+          <a class="active" href="https://rmlgc.github.io/dummyportfolio-vue/">Web</a>
+        </div>
+
       </section>
-      <section id="elContact" ref="elContact" class="content section-card">
-        <h2 class="text-h1"> Contact </h2>
+      <section id="elContact" ref="elContact" class="content section-card wrap-center">
+        <a href="mailto:rommelgarcia4b@gmail.com" class="active">
+          <h2 class="text-h3">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contact me &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h2>
+        </a>
       </section>
       <footer>
         <a href="https://linktr.ee/rommelgc" target="_blank">Rommel Garcia Coronado</a>
@@ -146,7 +292,7 @@ contact_mail
   </main>
 </template>
 
-<style lang="scss">
+<style scoped lang="scss">
 .decoration-none {
   text-decoration: none;
 }
@@ -192,9 +338,55 @@ contact_mail
   border-right-color: var(--link-color);
   border-bottom-color: var(--link-color);
   width: 60px;
+  max-width: 800px;
+
+  &:after {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    content: "";
+    border-top: 48px solid #ccc;
+    border-right: 58px solid transparent;
+    transition: max-width 0.5s cubic-bezier(.68, 0, .27, 1.50);
+    max-width: 60px;
+    width: 100vw;
+
+    .open & {
+      transition: max-width 1s cubic-bezier(.68, -0.55, .27, 1.25);
+      max-width: 800px;
+    }
+  }
+
+  &-title {
+    background-color: var(--link-background-color);
+    animation: BgNeonLink 4s linear infinite;
+  }
+
+  &-btn {
+    height: 50px;
+    width: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: var(--link-color);
+    margin: auto;
+    cursor: pointer;
+  }
+
+  &-content {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    @media screen and (max-width: 700px) {
+      //overflow-y: scroll;
+      max-height: 265px;
+    }
+    box-shadow: inset black 0px 0px 5px;
+  }
 
   &-full {
-    width: 100vw;
+    width: 90vw;
     color: var(--background-color);
     backdrop-filter: blur(5px);
     font-size: inherit;
@@ -210,11 +402,43 @@ contact_mail
     font-size: 0px;
     text-align: left;
     cursor: pointer;
+    flex: 1 0 100%;
+    min-height: 60px;
+    width: 100%;
+
+    &.active {
+      animation: BgNeonLink 4s linear infinite;
+    }
+
     .open & {
       gap: 20px;
       font-size: 1.5rem;
       justify-content: end;
     }
   }
+}
+
+@keyframes BgNeonLink {
+  0% {
+    border-radius: 5px;
+    box-shadow: -3px 3px 2px 1px var(--link-background-color-opacity),
+    -3px -3px 2px 1px var(--link-background-color-opacity),
+    3px 3px 2px 1px var(--link-background-color-opacity),
+    3px -3px 2px 1px var(--link-background-color-opacity);
+  }
+  50% {
+    box-shadow: -1px 1px 5px 3px var(--link-background-color-opacity),
+    -1px -1px 5px 3px var(--link-background-color-opacity),
+    1px 1px 5px 3px var(--link-background-color-opacity),
+    1px -1px 5px 3px var(--link-background-color-opacity);
+  }
+  100% {
+    box-shadow: -3px 3px 2px 1px var(--link-background-color-opacity),
+    -3px -3px 2px 1px var(--link-background-color-opacity),
+    3px 3px 2px 1px var(--link-background-color-opacity),
+    3px -3px 2px 1px var(--link-background-color-opacity);
+    border-radius: 5px;
+  }
+
 }
 </style>
--- src/components/Landing/WipPage.vue
+++ src/components/Landing/WipPage.vue
@@ -1,34 +1,70 @@
 <script setup lang="ts">
+import {ref} from 'vue'
 
+const open = ref(false)
 </script>
 <template>
-  <section class="wrap-center">
-    <div>
-      <h1 class="text--interact text-center">
-        <span class="text-increase">
-          W</span>
-        <span class="text-reduces">ork
-        </span>
-        <span class="text-increase">I</span>
-        <span class="text-reduces">n
-        </span>
-        <span class="text-increase">
-          P</span>
-        <span class="text-reduces">rogress
-        </span>
-      </h1>
-    </div>
+  <section class="wrap-center wrap-wip" @click="open = !open">
+    <span class="material-symbols-outlined">touch_app</span>
+    <h1 :class="`text--interact text-center ${open ? 'open' : ''}`">
+      <span :class="`${ open ? 'inline-block' : '' }`">
+        <span class="text--wip text-increase">W</span>
+        <span class="text--wip text-reduces">ork&nbsp;</span>
+      </span>
+      <span :class="`${ open ? 'inline-block' : '' }`">
+      <span class="text--wip text-increase">I</span>
+      <span class="text--wip text-reduces">n&nbsp;</span>
+      </span>
+      <span :class="`${ open ? 'inline-block' : '' }`">
+      <span class="text--wip text-increase">P</span>
+      <span class="text--wip text-reduces">rogress</span>
+      </span>
+    </h1>
+    <span class="material-symbols-outlined">touch_app</span>
   </section>
 </template>
 <style scoped lang="scss">
+$deep: 10;
+$textColor: var(--text-color-opacity);
+$textBorderColor: var(--link-color);
+$deepOpen: 3;
+$textColorOpen: var(--link-background-color-opacity);
+$textBorderColorOpen: var(--text-color);
+
+@function makelongshadow($deep, $color, $borderColor) {
+  $val: 0px 0px 3px $borderColor;
+  @for $i from 1 through ($deep - 1) {
+    $val: #{$val}, #{$i}px #{$i}px #{$color};
+  }
+  $val: #{$val}, #{$deep}px #{$deep}px 3px $borderColor;
+  @return $val;
+}
+
+@mixin longshadow($deep, $color, $border) {
+  text-shadow: makelongshadow($deep, $color, $border);
+}
+
 .text {
+  &--wip {
+    @include longshadow($deep, $textColor, $textBorderColor);
+    shape-rendering: optimizeSpeed;
+    color-rendering: optimizeSpeed;
+    text-rendering: geometricPrecision;
+
+    .open & {
+      @include longshadow($deepOpen, $textColorOpen, $textBorderColorOpen);
+    }
+  }
+
   &-increase {
-    font-size: 8rem;
-    transition: font-size 1s cubic-bezier(.68, -0.55, .27, 1.55);
+    font-size: 7.5rem;
+    transition: font-size 1s cubic-bezier(.68, -0.55, .27, 1.55),
+    text-shadow 1.5s cubic-bezier(.68, -0.55, .27, 1.55);
     font-weight: 900;
     animation: firstAnimationIncrease 3s cubic-bezier(.68, -0.55, .27, 1.55);
 
     .text--interact:hover &,
+    .text--interact.open &,
     &.text--interact:hover {
       font-size: 4.7rem;
     }
@@ -36,15 +72,29 @@
 
   &-reduces {
     font-size: 0;
-    transition: font-size 1s cubic-bezier(.68, -0.55, .27, 1.55);
+    transition: font-size 1s cubic-bezier(.68, -0.55, .27, 1.55),
+    text-shadow 1.5s cubic-bezier(.68, -0.55, .27, 1.55);
     font-weight: 900;
     animation: firstAnimationReduces 2.5s cubic-bezier(.68, -0.55, .27, 1.55);
 
     .text--interact:hover &,
     &.text--interact:hover {
-      font-size: 3.5rem;
+      @media (hover: hover) {
+        & {
+          font-size: 3.2rem;
+        }
+      }
     }
+
+    .text--interact.open & {
+      font-size: 3.2rem;
+    }
+
   }
+}
+
+.wrap-wip {
+  min-height: 60vh;
 }
 
 @keyframes firstAnimationIncrease {
@@ -58,7 +108,7 @@
     font-size: 7rem;
   }
   100% {
-    font-size: 8rem;
+    font-size: 7.5rem;
   }
 }
 
