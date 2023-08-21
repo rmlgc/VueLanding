801908ec45ce9c0c56cbc5b2a3aaaac008523b5c
rmlgc <rommelgarcia4b@gmail.com>
2023-08-21 20:39:35 +0100

    aLinks margins added

--- src/components/Landing/SectionPortfolio.vue
+++ src/components/Landing/SectionPortfolio.vue
@@ -8,14 +8,15 @@
           <h3>Amigaria WebApp</h3>
           <p>Amigaria is an accessible web platform for social integration.</p>
           <p>developed in group in EOI frontend develop course, doing Agile methods and publish git versions.</p>
-          <a class="active" href="https://amigaria.com/">Amigaria</a>
+          <a class="active" href="https://amigaria-dev.vercel.app/" target="_blank">Amigaria - Landing</a>
+          <a class="active" href="https://amigaria-app.vercel.app/login" target="_blank">Amigaria - WebApp</a>
         </div>
         <div>
           <h3>Vue Dummyportfolio</h3>
           <p>Work In Progress</p>
           <p>Simple Web App. render, connect to Rest Apis and list data</p>
           <p>Developed in Vue, using Quasar VueJs Components. for a simple mobile and desktop style design</p>
-          <a class="active" href="https://rmlgc.github.io/dummyportfolio-vue/">Web</a>
+          <a class="active" href="https://rmlgc.github.io/dummyportfolio-vue/" target="_blank">Web</a>
         </div>
 
       </section>
